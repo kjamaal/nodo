5491f28075aefa0a2aa48565f1f91a35a094af8e
Jamaal Kirkparick <kjamaal81@gmail.com>
2026-01-02 12:51:44 -0800

    fix: add basename to BrowserRouter for proper routing

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import Contact from './pages/Contact';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/nodo">
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
